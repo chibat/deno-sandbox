8826bea89c466ece7d55cea8adbc0da88c0cbfeb
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2020-07-13 08:57:59 +0900

    -

---
+++ src/http_client_test.ts
@@ -0,0 +1,15 @@
+import { exchange, Request } from "./http_client.ts";
+
+// normal
+const url =
+  "https://gist.githubusercontent.com/chibat/b207260420c1b85012036ffc6743f427/raw/16d7a15460df1d40596b2e6a151fd2604ea10afd/hello.txt";
+
+// chunk
+//const url = "https://github.com/";
+
+const request: Request = { method: "GET", url: url };
+const res = await exchange(request);
+console.log("Status: " + res.status);
+console.log("Body: " + res.body);
+
+// deno run -A http-client.ts
